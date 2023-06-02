961518637f1417e85d5e7bb90664180d4b497e29
Rajat Sharma <36663311+iamRajatSharma@users.noreply.github.com>
2023-06-03 00:33:08 +0530

    Database changes

--- DB/conn.js
+++ DB/conn.js
@@ -1,20 +1,20 @@
-var mysql = require('mysql2');
-var conn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'e-commerce',
-    // insecureAuth: true,
-    // multipleStatements: true
-});
+// var mysql = require('mysql2');
+// var conn = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'e-commerce',
+//     // insecureAuth: true,
+//     // multipleStatements: true
+// });
 
-conn.connect((err, res) => {
-    if (err) {
-        console.log(err)
-    }
-    else {
-        console.log("DB Success")
-    }
-});
+// conn.connect((err, res) => {
+//     if (err) {
+//         console.log(err)
+//     }
+//     else {
+//         console.log("DB Success")
+//     }
+// });
 
-module.exports = conn
+// module.exports = conn
